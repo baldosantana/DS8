68f133b65dd2e322a58ef8cbae2f262c49fe6090
baldosantana <baldosantana@gmail.com>
2023-09-24 18:20:40 -0500

    avance proy1

--- Proyecto1/Proyecto1/Form1.Designer.vb
+++ Proyecto1/Proyecto1/Form1.Designer.vb
@@ -22,30 +22,32 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
         Label1 = New Label()
-        TextBox1 = New TextBox()
-        TextBox2 = New TextBox()
-        TextBox3 = New TextBox()
-        TextBox4 = New TextBox()
-        TextBox5 = New TextBox()
-        TextBox6 = New TextBox()
-        TextBox7 = New TextBox()
+        vtxt_Valor1 = New TextBox()
+        vtxt_Valor2 = New TextBox()
+        vtxt_Valor3 = New TextBox()
+        vtxt_Valor4 = New TextBox()
+        vtxt_Valor5 = New TextBox()
+        vtxt_Valor6 = New TextBox()
+        vtxt_Valor7 = New TextBox()
         Label2 = New Label()
-        TextBox8 = New TextBox()
-        Button1 = New Button()
-        Button2 = New Button()
-        Button3 = New Button()
-        Button4 = New Button()
+        vtxt_Valor8 = New TextBox()
+        vbtn_Valor1 = New Button()
+        vbtn_Valor2 = New Button()
+        vbtn_Valor3 = New Button()
+        vbtn_Valor4 = New Button()
         Button5 = New Button()
         Button6 = New Button()
         Button7 = New Button()
         Button8 = New Button()
-        Button9 = New Button()
-        TextBox9 = New TextBox()
+        btn_Iniciar = New Button()
+        vtxt_Valor9 = New TextBox()
         Button10 = New Button()
-        Button11 = New Button()
+        btn_BorrarTodo = New Button()
         Label3 = New Label()
-        TextBox10 = New TextBox()
+        txt_Contador = New TextBox()
+        tmr_Contador = New Timer(components)
         SuspendLayout()
         ' 
         ' Label1
@@ -58,54 +60,54 @@ Partial Class Form1
         Label1.TabIndex = 0
         Label1.Text = "Proyecto No.1 Reto Matematico"
         ' 
-        ' TextBox1
+        ' vtxt_Valor1
         ' 
-        TextBox1.Location = New Point(51, 177)
-        TextBox1.Name = "TextBox1"
-        TextBox1.Size = New Size(33, 23)
-        TextBox1.TabIndex = 1
+        vtxt_Valor1.Location = New Point(51, 177)
+        vtxt_Valor1.Name = "vtxt_Valor1"
+        vtxt_Valor1.Size = New Size(33, 23)
+        vtxt_Valor1.TabIndex = 1
         ' 
-        ' TextBox2
+        ' vtxt_Valor2
         ' 
-        TextBox2.Location = New Point(90, 177)
-        TextBox2.Name = "TextBox2"
-        TextBox2.Size = New Size(33, 23)
-        TextBox2.TabIndex = 2
+        vtxt_Valor2.Location = New Point(90, 177)
+        vtxt_Valor2.Name = "vtxt_Valor2"
+        vtxt_Valor2.Size = New Size(33, 23)
+        vtxt_Valor2.TabIndex = 2
         ' 
-        ' TextBox3
+        ' vtxt_Valor3
         ' 
-        TextBox3.Location = New Point(129, 177)
-        TextBox3.Name = "TextBox3"
-        TextBox3.Size = New Size(33, 23)
-        TextBox3.TabIndex = 3
+        vtxt_Valor3.Location = New Point(129, 177)
+        vtxt_Valor3.Name = "vtxt_Valor3"
+        vtxt_Valor3.Size = New Size(33, 23)
+        vtxt_Valor3.TabIndex = 3
         ' 
-        ' TextBox4
+        ' vtxt_Valor4
         ' 
-        TextBox4.Location = New Point(168, 177)
-        TextBox4.Name = "TextBox4"
-        TextBox4.Size = New Size(33, 23)
-        TextBox4.TabIndex = 4
+        vtxt_Valor4.Location = New Point(168, 177)
+        vtxt_Valor4.Name = "vtxt_Valor4"
+        vtxt_Valor4.Size = New Size(33, 23)
+        vtxt_Valor4.TabIndex = 4
         ' 
-        ' TextBox5
+        ' vtxt_Valor5
         ' 
-        TextBox5.Location = New Point(207, 177)
-        TextBox5.Name = "TextBox5"
-        TextBox5.Size = New Size(33, 23)
-        TextBox5.TabIndex = 5
+        vtxt_Valor5.Location = New Point(207, 177)
+        vtxt_Valor5.Name = "vtxt_Valor5"
+        vtxt_Valor5.Size = New Size(33, 23)
+        vtxt_Valor5.TabIndex = 5
         ' 
-        ' TextBox6
+        ' vtxt_Valor6
         ' 
-        TextBox6.Location = New Point(246, 177)
-        TextBox6.Name = "TextBox6"
-        TextBox6.Size = New Size(33, 23)
-        TextBox6.TabIndex = 6
+        vtxt_Valor6.Location = New Point(246, 177)
+        vtxt_Valor6.Name = "vtxt_Valor6"
+        vtxt_Valor6.Size = New Size(33, 23)
+        vtxt_Valor6.TabIndex = 6
         ' 
-        ' TextBox7
+        ' vtxt_Valor7
         ' 
-        TextBox7.Location = New Point(285, 177)
-        TextBox7.Name = "TextBox7"
-        TextBox7.Size = New Size(33, 23)
-        TextBox7.TabIndex = 7
+        vtxt_Valor7.Location = New Point(285, 177)
+        vtxt_Valor7.Name = "vtxt_Valor7"
+        vtxt_Valor7.Size = New Size(33, 23)
+        vtxt_Valor7.TabIndex = 7
         ' 
         ' Label2
         ' 
@@ -116,44 +118,44 @@ Partial Class Form1
         Label2.TabIndex = 8
         Label2.Text = "="
         ' 
-        ' TextBox8
+        ' vtxt_Valor8
         ' 
-        TextBox8.Location = New Point(345, 177)
-        TextBox8.Name = "TextBox8"
-        TextBox8.Size = New Size(33, 23)
-        TextBox8.TabIndex = 9
+        vtxt_Valor8.Location = New Point(345, 177)
+        vtxt_Valor8.Name = "vtxt_Valor8"
+        vtxt_Valor8.Size = New Size(33, 23)
+        vtxt_Valor8.TabIndex = 9
         ' 
-        ' Button1
+        ' vbtn_Valor1
         ' 
-        Button1.Location = New Point(102, 245)
-        Button1.Name = "Button1"
-        Button1.Size = New Size(42, 23)
-        Button1.TabIndex = 10
-        Button1.UseVisualStyleBackColor = True
+        vbtn_Valor1.Location = New Point(102, 245)
+        vbtn_Valor1.Name = "vbtn_Valor1"
+        vbtn_Valor1.Size = New Size(42, 23)
+        vbtn_Valor1.TabIndex = 10
+        vbtn_Valor1.UseVisualStyleBackColor = True
         ' 
-        ' Button2
+        ' vbtn_Valor2
         ' 
-        Button2.Location = New Point(150, 245)
-        Button2.Name = "Button2"
-        Button2.Size = New Size(42, 23)
-        Button2.TabIndex = 11
-        Button2.UseVisualStyleBackColor = True
+        vbtn_Valor2.Location = New Point(150, 245)
+        vbtn_Valor2.Name = "vbtn_Valor2"
+        vbtn_Valor2.Size = New Size(42, 23)
+        vbtn_Valor2.TabIndex = 11
+        vbtn_Valor2.UseVisualStyleBackColor = True
         ' 
-        ' Button3
+        ' vbtn_Valor3
         ' 
-        Button3.Location = New Point(198, 245)
-        Button3.Name = "Button3"
-        Button3.Size = New Size(42, 23)
-        Button3.TabIndex = 12
-        Button3.UseVisualStyleBackColor = True
+        vbtn_Valor3.Location = New Point(198, 245)
+        vbtn_Valor3.Name = "vbtn_Valor3"
+        vbtn_Valor3.Size = New Size(42, 23)
+        vbtn_Valor3.TabIndex = 12
+        vbtn_Valor3.UseVisualStyleBackColor = True
         ' 
-        ' Button4
+        ' vbtn_Valor4
         ' 
-        Button4.Location = New Point(246, 245)
-        Button4.Name = "Button4"
-        Button4.Size = New Size(42, 23)
-        Button4.TabIndex = 13
-        Button4.UseVisualStyleBackColor = True
+        vbtn_Valor4.Location = New Point(246, 245)
+        vbtn_Valor4.Name = "vbtn_Valor4"
+        vbtn_Valor4.Size = New Size(42, 23)
+        vbtn_Valor4.TabIndex = 13
+        vbtn_Valor4.UseVisualStyleBackColor = True
         ' 
         ' Button5
         ' 
@@ -161,7 +163,6 @@ Partial Class Form1
         Button5.Name = "Button5"
         Button5.Size = New Size(42, 23)
         Button5.TabIndex = 14
-        Button5.Text = "+"
         Button5.UseVisualStyleBackColor = True
         ' 
         ' Button6
@@ -170,7 +171,6 @@ Partial Class Form1
         Button6.Name = "Button6"
         Button6.Size = New Size(42, 23)
         Button6.TabIndex = 15
-        Button6.Text = "-"
         Button6.UseVisualStyleBackColor = True
         ' 
         ' Button7
@@ -179,7 +179,6 @@ Partial Class Form1
         Button7.Name = "Button7"
         Button7.Size = New Size(42, 23)
         Button7.TabIndex = 16
-        Button7.Text = "*"
         Button7.UseVisualStyleBackColor = True
         ' 
         ' Button8
@@ -188,24 +187,23 @@ Partial Class Form1
         Button8.Name = "Button8"
         Button8.Size = New Size(42, 23)
         Button8.TabIndex = 17
-        Button8.Text = "/"
         Button8.UseVisualStyleBackColor = True
         ' 
-        ' Button9
+        ' btn_Iniciar
         ' 
-        Button9.Location = New Point(447, 152)
-        Button9.Name = "Button9"
-        Button9.Size = New Size(75, 23)
-        Button9.TabIndex = 18
-        Button9.Text = "Iniciar"
-        Button9.UseVisualStyleBackColor = True
+        btn_Iniciar.Location = New Point(447, 152)
+        btn_Iniciar.Name = "btn_Iniciar"
+        btn_Iniciar.Size = New Size(75, 23)
+        btn_Iniciar.TabIndex = 18
+        btn_Iniciar.Text = "Iniciar"
+        btn_Iniciar.UseVisualStyleBackColor = True
         ' 
-        ' TextBox9
+        ' vtxt_Valor9
         ' 
-        TextBox9.Location = New Point(345, 206)
-        TextBox9.Name = "TextBox9"
-        TextBox9.Size = New Size(33, 23)
-        TextBox9.TabIndex = 19
+        vtxt_Valor9.Location = New Point(345, 206)
+        vtxt_Valor9.Name = "vtxt_Valor9"
+        vtxt_Valor9.Size = New Size(33, 23)
+        vtxt_Valor9.TabIndex = 19
         ' 
         ' Button10
         ' 
@@ -216,14 +214,14 @@ Partial Class Form1
         Button10.Text = "Borrar"
         Button10.UseVisualStyleBackColor = True
         ' 
-        ' Button11
+        ' btn_BorrarTodo
         ' 
-        Button11.Location = New Point(447, 258)
-        Button11.Name = "Button11"
-        Button11.Size = New Size(86, 23)
-        Button11.TabIndex = 21
-        Button11.Text = "Borrar Todo"
-        Button11.UseVisualStyleBackColor = True
+        btn_BorrarTodo.Location = New Point(447, 258)
+        btn_BorrarTodo.Name = "btn_BorrarTodo"
+        btn_BorrarTodo.Size = New Size(86, 23)
+        btn_BorrarTodo.TabIndex = 21
+        btn_BorrarTodo.Text = "Borrar Todo"
+        btn_BorrarTodo.UseVisualStyleBackColor = True
         ' 
         ' Label3
         ' 
@@ -234,41 +232,45 @@ Partial Class Form1
         Label3.TabIndex = 22
         Label3.Text = "Reloj"
         ' 
-        ' TextBox10
+        ' txt_Contador
         ' 
-        TextBox10.Location = New Point(447, 115)
-        TextBox10.Name = "TextBox10"
-        TextBox10.Size = New Size(31, 23)
-        TextBox10.TabIndex = 23
+        txt_Contador.Location = New Point(447, 115)
+        txt_Contador.Name = "txt_Contador"
+        txt_Contador.Size = New Size(31, 23)
+        txt_Contador.TabIndex = 23
+        ' 
+        ' tmr_Contador
+        ' 
+        tmr_Contador.Interval = 1000
         ' 
         ' Form1
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(800, 450)
-        Controls.Add(TextBox10)
+        Controls.Add(txt_Contador)
         Controls.Add(Label3)
-        Controls.Add(Button11)
+        Controls.Add(btn_BorrarTodo)
         Controls.Add(Button10)
-        Controls.Add(TextBox9)
-        Controls.Add(Button9)
+        Controls.Add(vtxt_Valor9)
+        Controls.Add(btn_Iniciar)
         Controls.Add(Button8)
         Controls.Add(Button7)
         Controls.Add(Button6)
         Controls.Add(Button5)
-        Controls.Add(Button4)
-        Controls.Add(Button3)
-        Controls.Add(Button2)
-        Controls.Add(Button1)
-        Controls.Add(TextBox8)
+        Controls.Add(vbtn_Valor4)
+        Controls.Add(vbtn_Valor3)
+        Controls.Add(vbtn_Valor2)
+        Controls.Add(vbtn_Valor1)
+        Controls.Add(vtxt_Valor8)
         Controls.Add(Label2)
-        Controls.Add(TextBox7)
-        Controls.Add(TextBox6)
-        Controls.Add(TextBox5)
-        Controls.Add(TextBox4)
-        Controls.Add(TextBox3)
-        Controls.Add(TextBox2)
-        Controls.Add(TextBox1)
+        Controls.Add(vtxt_Valor7)
+        Controls.Add(vtxt_Valor6)
+        Controls.Add(vtxt_Valor5)
+        Controls.Add(vtxt_Valor4)
+        Controls.Add(vtxt_Valor3)
+        Controls.Add(vtxt_Valor2)
+        Controls.Add(vtxt_Valor1)
         Controls.Add(Label1)
         Name = "Form1"
         Text = "Form1"
@@ -277,27 +279,28 @@ Partial Class Form1
     End Sub
 
     Friend WithEvents Label1 As Label
-    Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents TextBox2 As TextBox
-    Friend WithEvents TextBox3 As TextBox
-    Friend WithEvents TextBox4 As TextBox
-    Friend WithEvents TextBox5 As TextBox
-    Friend WithEvents TextBox6 As TextBox
-    Friend WithEvents TextBox7 As TextBox
+    Friend WithEvents vtxt_Valor1 As TextBox
+    Friend WithEvents vtxt_Valor2 As TextBox
+    Friend WithEvents vtxt_Valor3 As TextBox
+    Friend WithEvents vtxt_Valor4 As TextBox
+    Friend WithEvents vtxt_Valor5 As TextBox
+    Friend WithEvents vtxt_Valor6 As TextBox
+    Friend WithEvents vtxt_Valor7 As TextBox
     Friend WithEvents Label2 As Label
-    Friend WithEvents TextBox8 As TextBox
-    Friend WithEvents Button1 As Button
-    Friend WithEvents Button2 As Button
-    Friend WithEvents Button3 As Button
-    Friend WithEvents Button4 As Button
+    Friend WithEvents vtxt_Valor8 As TextBox
+    Friend WithEvents vbtn_Valor1 As Button
+    Friend WithEvents vbtn_Valor2 As Button
+    Friend WithEvents vbtn_Valor3 As Button
+    Friend WithEvents vbtn_Valor4 As Button
     Friend WithEvents Button5 As Button
     Friend WithEvents Button6 As Button
     Friend WithEvents Button7 As Button
     Friend WithEvents Button8 As Button
-    Friend WithEvents Button9 As Button
-    Friend WithEvents TextBox9 As TextBox
+    Friend WithEvents btn_Iniciar As Button
+    Friend WithEvents vtxt_Valor9 As TextBox
     Friend WithEvents Button10 As Button
-    Friend WithEvents Button11 As Button
+    Friend WithEvents btn_BorrarTodo As Button
     Friend WithEvents Label3 As Label
-    Friend WithEvents TextBox10 As TextBox
+    Friend WithEvents txt_Contador As TextBox
+    Friend WithEvents tmr_Contador As Timer
 End Class
--- Proyecto1/Proyecto1/Form1.vb
+++ Proyecto1/Proyecto1/Form1.vb
@@ -1,4 +1,67 @@
 ﻿Public Class Form1
+    Dim vbtn_Valor(4) As Button
+
+    Public Sub New()
+
+        ' Esta llamada es exigida por el diseñador.
+        InitializeComponent()
+
+        ' Agregue cualquier inicialización después de la llamada a InitializeComponent().
+        vbtn_Valor(1) = vbtn_Valor1
+        vbtn_Valor(2) = vbtn_Valor2
+        vbtn_Valor(3) = vbtn_Valor3
+        vbtn_Valor(4) = vbtn_Valor4
+    End Sub
+
+    Dim vbtn_oper(4) As Button
+
+    Public Sub New_Oper()
+
+        ' Esta llamada es exigida por el diseñador.
+        InitializeComponent()
+
+        ' Agregue cualquier inicialización después de la llamada a InitializeComponent().
+        vbtn_oper(1).Text = "+"
+        vbtn_oper(2).Text = "-"
+        vbtn_oper(3).Text = "*"
+        vbtn_oper(4).Text = "/"
+    End Sub
+
+    Dim vtxt_Valor(9) As TextBox
+
+    Public Sub New_txt()
+
+        ' Esta llamada es exigida por el diseñador.
+        InitializeComponent()
+
+        ' Agregue cualquier inicialización después de la llamada a InitializeComponent().
+        vtxt_Valor(1) = vtxt_Valor1
+        vtxt_Valor(2) = vtxt_Valor2
+        vtxt_Valor(3) = vtxt_Valor3
+        vtxt_Valor(4) = vtxt_Valor4
+        vtxt_Valor(5) = vtxt_Valor5
+        vtxt_Valor(6) = vtxt_Valor6
+        vtxt_Valor(7) = vtxt_Valor7
+        vtxt_Valor(8) = vtxt_Valor8
+        vtxt_Valor(9) = vtxt_Valor9
+    End Sub
+
+    Private Sub DisableOper()
+
+        For i = 1 To 4
+            vbtn_oper(i).Enabled = False
+        Next
+    End Sub
+
+    Private Sub BorrarTodo()
+
+        For i = 1 To 4
+            vtxt_Valor(i).Text = ""
+        Next
+    End Sub
+
+
+
     Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
 
     End Sub
@@ -7,19 +70,47 @@
 
     End Sub
 
-    Private Sub TextBox8_TextChanged(sender As Object, e As EventArgs) Handles TextBox8.TextChanged
+    Private Sub TextBox8_TextChanged(sender As Object, e As EventArgs) Handles vtxt_Valor8.TextChanged
 
     End Sub
 
-    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles vbtn_Valor3.Click
 
     End Sub
 
-    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles btn_Iniciar.Click
+        Dim rnd As New Random()
+
+        For i = 1 To 4
+            vbtn_Valor(i).Text = rnd.Next(1, 5)
+        Next
+        tmr_Contador.Enabled = True
+        btn_Iniciar.Enabled = False
+
 
     End Sub
 
     Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
 
+    End Sub
+
+    Private Sub tmr_Contador_Tick(sender As Object, e As EventArgs) Handles tmr_Contador.Tick
+        txt_Contador.Text = Val(txt_Contador.Text) + 1
+    End Sub
+
+    Private Sub vbtn_Valor1_Click(sender As Object, e As EventArgs) Handles vbtn_Valor1.Click
+        vtxt_Valor1.Text = vbtn_Valor1.Text
+    End Sub
+
+    Private Sub vbtn_Valor2_Click(sender As Object, e As EventArgs) Handles vbtn_Valor2.Click
+
+    End Sub
+
+    Private Sub vtxt_Valor3_TextChanged(sender As Object, e As EventArgs) Handles vtxt_Valor3.TextChanged
+
+    End Sub
+
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles btn_BorrarTodo.Click
+        BorrarTodo()
     End Sub
 End Class
